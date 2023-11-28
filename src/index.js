d8ff80f65543f19f9d276bba7838ab9c4f22cdbd
zaquari_andl <zaquariandl043@gmail.com>
2023-11-28 10:23:29 -0600

    completed cc

--- src/index.js
+++ src/index.js
@@ -1,52 +1,80 @@
 let currentMovie
 
+const movieList = document.querySelector('#movie-list')
 
-const movieDetailImage = document.querySelector("div#movie-info img#detail-image")
-const movieDetailTitle = document.querySelector("div#movie-info h1#title")
-const movieDetailYearReleased = document.querySelector("div#movie-info h3#year-released")
-const movieDetailDescription = document.querySelector("div#movie-info p#description")
-const movieDetailWatched = document.querySelector("div#movie-info button#watched")
-const movieDetailBlood = document.querySelector("div#movie-info #amount")
+const detailMovieImage = document.querySelector('#detail-image')
+const detailMovieTitle = document.querySelector('#title')
+const detailMovieYearReleased = document.querySelector('#year-released')
+const detailMovieDescription = document.querySelector('#description')
+const detailMovieWatched = document.querySelector('#watched')
+const detailMovieBloodAmount = document.querySelector('#amount')
+
+const bloodForm = document.querySelector('#blood-form')
+const bloodInput = document.querySelector('#blood-amount')
 
 
-    fetch('http://localhost:3000/movies')
-        .then(response => response.json())
-        .then(movieArr => {
-            movieArr.map(eachMovie => {
-                createImageMenu(eachMovie)
-            })
-            renderMovieDetail(movieArr[0])  
+const url = 'http://localhost:3000/movies'
 
-            toggleWatchedButton()
+function getMovies() {
+    fetch(url)
+    .then(res => res.json())
+    .then(movieArr => {
+        
+        movieDisplay(movieArr[0])
+
+        toggleWatchedButton()
+
+        bloodTotal()
+
+        movieArr.map(eachMovie => {
+            displayMovieMenu(eachMovie)
         })
 
-function createImageMenu(movie) {
-    const movieList = document.querySelector('#movie-list')
-    const image = document.createElement('img')
-    image.src = movie.image
-    movieList.appendChild(image)
 
-    image.addEventListener('click', () => {
-         renderMovieDetail(movie)
     })
 }
 
-const renderMovieDetail = (movie) =>{
-    currentMovie = movie
+const displayMovieMenu = (movie) => {
+    const imageElement = document.createElement('img')
+    imageElement.src = movie.image
+    movieList.appendChild(imageElement)
 
-    movieDetailImage.src = movie.image
-    movieDetailTitle.innerText = movie.title
-    movieDetailYearReleased.innerText = movie.release_year
-    movieDetailDescription.innerText = movie.description 
-    movieDetailWatched.innerText = movie.watched ? "Watched" : "Unwatched"
-    movieDetailBlood.innerText = movie.blood_amount
+    imageElement.addEventListener('click', () => {
+        movieDisplay(movie)
+    })
+}
+
+const movieDisplay = (movie) => {
+    currentMovie = movie
+    
+    detailMovieImage.src = movie.image
+    detailMovieTitle.textContent = movie.title
+    detailMovieYearReleased.textContent = movie.release_year
+    detailMovieDescription.textContent = movie.description
+    detailMovieWatched.textContent = movie.watched ? "Watched" : "Unwatched"
+    detailMovieBloodAmount.textContent = movie.blood_amount
 }
 
 const toggleWatchedButton = () => {
-        movieDetailWatched.addEventListener('click', () => {
-            currentMovie.watched = !currentMovie.watched
-            movieDetailWatched.textContent = currentMovie.watched ? "Watched" : "Unwatched"
 
-        })
-
+    detailMovieWatched.addEventListener('click', () => {
+        console.log("clicked")
+    currentMovie.watched = !currentMovie.watched
+    detailMovieWatched.textContent = currentMovie.watched ? "Watched" : "Unwatched"
+    })
 }
+
+const bloodTotal = () => {
+    bloodForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+
+        const sum = Number(bloodInput.value) + Number(detailMovieBloodAmount.textContent)
+        // console.log
+        detailMovieBloodAmount.textContent = sum
+
+        bloodInput.value = ""
+
+    })
+}
+
+getMovies()
